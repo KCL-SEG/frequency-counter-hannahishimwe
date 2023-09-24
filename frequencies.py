5551f57cb537df3855580ce26969a9ed18bc3761
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2023-09-24 19:04:31 +0100

    Clean up

--- frequencies.py
+++ frequencies.py
@@ -1,9 +1,8 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-#if the thing is already in there, value +1
+
 def frequencies(items):
     frequencies = {}
-    
     for item in items:
         if frequencies.get(str(item)):
             frequencies.update({str(item): (frequencies.get(str(item)) + 1)})
