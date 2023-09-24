649d6371c1f98a19467ee88261381912927db040
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2023-09-24 19:00:30 +0100

    Add and test solution to method

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
+#if the thing is already in there, value +1
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for item in items:
+        if frequencies.get(str(item)):
+            frequencies.update({str(item): (frequencies.get(str(item)) + 1)})
+        else:
+            frequencies[str(item)] = 1
     return frequencies
